0022870650ab02ffc94304eae627a668976bdf70
Tiago Temporin <tiaguinhocab@gmail.com>
2017-05-23 16:05:09 -0300

    test validation for int

--- required.go
+++ required.go
@@ -1,27 +1,84 @@
 package required
 
 import (
-	"fmt"
 	"reflect"
+	"fmt"
 )
 
 //
 type Message struct {
-	Field   string `json:"field"`
-	Message string `json:"message"`
+	Field   string `json:"field" xml:"field"`
+	Message string `json:"message" xml:"message"`
 }
 
 //
-func Validate(v interface{}) (messages []Message, err error) {
-	numFields := reflect.ValueOf(v).NumField()
-	if numFields > 0 {
-		messages = make([]Message, 0)
+func Validate(v interface{}) ([]Message, error) {
+	return checkFields(reflect.ValueOf(v))
+}
 
-		for i := 0; i < numFields; i++ {
-			if msg, ok := reflect.TypeOf(v).Field(i).Tag.Lookup("required"); ok {
-				fmt.Println(msg)
+//
+func checkFields(v reflect.Value) (messages []Message, err error) {
+	for i := 0; i < v.NumField(); i++ {
+		if v.Field(i).Kind() != reflect.Ptr && v.Field(i).Kind() != reflect.Struct {
+			if msg, ok := v.Type().Field(i).Tag.Lookup("required"); ok {
+				if isEmpty(v.Field(i)) {
+					message := Message{
+						Field:   getFieldName(v.Type().Field(i)),
+						Message: "this field is required",
+					}
+
+					if msg != "-" {
+						message.Message = msg
+					}
+
+					messages = append(messages, message)
+				}
+			}
+		} else {
+			s, err := checkFields(v.Field(i))
+			if err != nil {
+				messages = append(messages, s...)
 			}
 		}
+	}
+
+	if len(messages) > 0 {
+		err = fmt.Errorf("%d fields are required", len(messages))
+	}
+	
+	return
+}
+
+//
+func isEmpty(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return v.Uint() == 0
+
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+
+	case reflect.Complex64, reflect.Complex128:
+		return v.Complex() == 0
+
+	case reflect.String:
+		return v.String() == ""
+	}
+
+	return false
+}
+
+//
+func getFieldName(f reflect.StructField) (s string) {
+	if t, ok := f.Tag.Lookup("json"); ok {
+		s = t
+	} else if t, ok := f.Tag.Lookup("xml"); ok {
+		s = t
+	} else {
+		s = f.Name
 	}
 
 	return
--- required_test.go
+++ required_test.go
@@ -1,27 +1,56 @@
 package required
 
 import (
-	"fmt"
 	"testing"
+	"reflect"
 )
-
-//
-type S struct {
-	StringDefault string `required:"-"`
-	StringCustom  string `required:"wow! this is empty"`
-	IntDefault    int32  `required:"-"`
-	IntCustom     int32  `required:"where is the number?"`
-}
 
 var (
-	v = S{}
-
-	stringCusto = "wow! this is empty"
+	defaultMessage = "this field is required"
 )
 
+type I struct {
+	Default    int  `json:"default" required:"-"`
+	Custom     int  `required:"where is the number?"`
+}
+
 //
-func TestValidate(t *testing.T) {
+func TestValidateInt(t *testing.T) {
+	v := I{}
+
+	dm := Message {
+		Field: "default",
+		Message: defaultMessage,
+	}
+
+	cm := Message{
+		Field: "Custom",
+		Message: "where is the number?",
+	}
+
 	if msgs, err := Validate(v); err != nil {
-		fmt.Println(msgs)
+		arr := make([]Message, 0)
+		arr = append(arr, dm, cm)
+
+		if !reflect.DeepEqual(arr, msgs) {
+			t.Errorf("\n expected: %+v \n got: %+v", arr, msgs)
+		}
+	}
+
+	v.Default = 100
+	v.Custom = 0
+	if msgs, err := Validate(v); err != nil {
+		arr := make([]Message, 0)
+		arr = append(arr, cm)
+
+		if !reflect.DeepEqual(arr, msgs) {
+			t.Errorf("\n expected: %+v \n got: %+v", arr, msgs)
+		}
+	}
+
+	v.Default = 100
+	v.Custom = 100
+	if msgs, err := Validate(v); err != nil {
+		t.Errorf("\n no error message expected \n got: %+v", msgs)
 	}
 }
